722439eb580a87ed4af5160f9f72f46d0e9ce92d
Musiki8213 <sithomolamusiki@gmail.com>
2025-07-10 16:02:01 +0200

    html

---
+++ script.js
@@ -0,0 +1,65 @@
+const modal = document.getElementById("javaPower");
+const openB = document.getElementById("powerOpen");
+const closeB =document.getElementById("powerClose");
+
+openB.onclick = function(){
+    modal.style.display = "block";
+}
+closeB.onclick = function(){
+    modal.style.display = "none";
+}
+
+const modal2 = document.getElementById("javaCPU");
+const openB2 = document.getElementById("cpuOpen");
+const closeB2 =document.getElementById("cpuClose");
+
+openB2.onclick = function(){
+    modal2.style.display = "block";
+}
+closeB2.onclick = function(){
+    modal2.style.display = "none";
+}
+
+const modal3 = document.getElementById("javaRam");
+const openB3 = document.getElementById("ramOpen");
+const closeB3 =document.getElementById("ramClose");
+
+openB3.onclick = function(){
+    modal3.style.display = "block";
+}
+closeB3.onclick = function(){
+    modal3.style.display = "none";
+}
+
+const modal4 = document.getElementById("javaRom");
+const openB4 = document.getElementById("romOpen");
+const closeB4 =document.getElementById("romClose");
+
+openB4.onclick = function(){
+    modal4.style.display = "block";
+}
+closeB4.onclick = function(){
+    modal4.style.display = "none";
+}
+
+const modal5 = document.getElementById("javaCmos");
+const openB5 = document.getElementById("cmosOpen");
+const closeB5 =document.getElementById("cmosClose");
+
+openB5.onclick = function(){
+    modal5.style.display = "block";
+}
+closeB5.onclick = function(){
+    modal5.style.display = "none";
+}
+
+const modal6 = document.getElementById("javaHdd");
+const openB6 = document.getElementById("hddOpen");
+const closeB6 =document.getElementById("hddClose");
+
+openB6.onclick = function(){
+    modal6.style.display = "block";
+}
+closeB6.onclick = function(){
+    modal6.style.display = "none";
+}
